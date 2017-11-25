0ec0e49acac0448bf45dfb2f3a0dc3f16d594485
chenrubao0047 <410618634@qq.com>
2017-11-26 00:04:46 +0800

    完成数据模型的设计

--- django_cms/settings.py
+++ django_cms/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'cms.UserProfile'
 
 # Application definition
 
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'django_cms.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cms',
+        'USER': 'root',
+        'PASSWORD': 'zhanshi123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
